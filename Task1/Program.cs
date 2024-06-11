4a91ffa82918f66a9dd17556f09be1f299c2f330
Евгений Иванов <evgen.iv.csm94@gmail.com>
2024-06-11 22:30:09 +0300

    add some variables

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, world!");
+﻿// Первая лекция по языку C#
+// Для вывода объекта в терминал, используем Console.WriteLine()
+//Console.WriteLine("Hello, world!");
+
+int value = 100;        // Задаем тип, имя и значение переменной
+Console.WriteLine(value);
+
+// Виды переменных
+// int = -2*10**31 ... 2*10**31
+// long = -2*10**63 ... 2*10**63
+// float = ±1.5*10**-45 ... ±3.4*10**38
+// double = ±5*10**-324 ... ±1.7*10**308
+// char = 0 ... 65535    символы
+// bool = true or false
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
