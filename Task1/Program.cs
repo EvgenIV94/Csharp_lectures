af1051e7dd2ec4de55dba7eb3837a81c24c31a1a
Евгений Иванов <evgen.iv.csm94@gmail.com>
2024-06-12 00:01:47 +0300

    Добавили арифметические действия с переменными

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,10 @@
 ﻿// Первая лекция по языку C#
+// прежде чем начать работу с текстовым файлом, необходимо ввести в терминале 'dotnet new console', а перед этим выбрать репозиторий с этим файлом
 // Для вывода объекта в терминал, используем Console.WriteLine()
 //Console.WriteLine("Hello, world!");
 
-int value = 100;        // Задаем тип, имя и значение переменной
-Console.WriteLine(value);
+// int value = 100;        // Задаем тип, имя и значение переменной
+// Console.WriteLine(value);
 
 // Виды переменных
 // int = -2*10**31 ... 2*10**31
@@ -13,10 +14,27 @@ Console.WriteLine(value);
 // char = 0 ... 65535    символы
 // bool = true or false
 
-double d = 5.55;
-char c = '!';
-bool b = true;
+// double d = 5.55;
+// char c = '!';
+// bool b = true;
 
-Console.WriteLine(d);
-Console.WriteLine(c);
-Console.WriteLine(b);
+// Console.WriteLine(d);
+// Console.WriteLine(c);
+// Console.WriteLine(b);
+
+// int a = 5;
+// int b = 6;
+// int sum = a + b;        // необходимо каждой переменной присваивать тип данных
+// Console.WriteLine(sum); 
+
+// long a = 6;
+// int b = 7;
+// long sum = a + b;   // если мы складываем разные типы данных, то тип суммы будет более расширенный тип данных. Например int + long = long
+// Console.WriteLine(sum);
+
+
+// double a = 5.5;
+// int b = 2;
+// double sum = a + b;     // сумма будет типа double
+// Console.WriteLine(sum);
+
